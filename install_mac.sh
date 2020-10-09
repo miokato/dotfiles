a183873f12524b038826c930922b60e25225d17b
miokato <miokato@me.com>
2020-10-09 14:01:23 +0900

    fix

--- install_mac.sh
+++ install_mac.sh
@@ -2,7 +2,7 @@
 
 # For mac
 
-ln -s ./.vimrc ~/.vimrc
-ln -s ./.vim ~/.vim
+ln -s ~/Develop/dotfiles/.vimrc ~/.vimrc
+ln -s ~/Develop/dotfiles/.vim ~/.vim
 ln -s ./settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s ./keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
