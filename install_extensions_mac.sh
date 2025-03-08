a7db5f786c0919d950fb1b2c91daf39ce8528921
miokato <miokato@me.com>
2025-03-08 17:34:24 +0900

    feat: add Windsurf extensions management
    - Add list of Windsurf extensions in extensions.txt
    - Update install_extensions_mac.sh to handle both VSCode and Windsurf extensions
    - Add proper error handling and progress messages

--- install_extensions_mac.sh
+++ install_extensions_mac.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+# Install VSCode extensions
+echo "Installing VSCode extensions..."
 cat ./extensions | while read line
 do
-  code --install-extensions $line
+  code --install-extension "$line"
 done
+
+# Install Windsurf extensions
+echo "Installing Windsurf extensions..."
+cat ./windsurf/extensions.txt | while read line
+do
+  windsurf --install-extension "$line"
+done
+
+echo "All extensions have been installed!"
