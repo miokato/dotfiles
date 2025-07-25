fbddc8d4cbd0acc97346ae8afcbbd7e46c8be9b3
miokato <miokato@me.com>
2025-07-25 11:34:03 +0900

    fix to remove windsurf settings in install_mac.sh.

--- install_mac.sh
+++ install_mac.sh
@@ -13,10 +13,6 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -sf ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 
-# windsurf
-ln -sf ~/dotfiles/windsurf/settings.json ~/.config/windsurf/settings.json
-ln -sf ~/dotfiles/windsurf/keybindings.json ~/.config/windsurf/keybindings.json
-
 # claude
 ln -sf ~/dotfiles/claude/settings.json ~/.claude/settings.json
 ln -sf ~/dotfiles/claude/CLAUDE.md ~/.claude/CLAUDE.md
